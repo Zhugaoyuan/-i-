fb0ea2fc2e8db2211b0ec02984f585699dd0690d
Administrator <Administrator@PC201906051206>
2019-06-12 20:16:38 +0800

    rty

--- 消息队列/Day16/Program.cs
+++ 消息队列/Day16/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 using System.Messaging;
 namespace Day16
 {
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
